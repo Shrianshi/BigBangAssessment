6f3dcffbd914b362e1f65130cbb39ee880df2946
Shrianshi <shrianshi.kanini@gmail.com>
2023-05-26 12:59:36 +0530

    Modified appsettings.json and program.cs

--- BigBangAssessment/Program.cs
+++ BigBangAssessment/Program.cs
@@ -1,8 +1,32 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
+using BigBangAssessment.Auth;
+using BigBangAssessment.Models;
+using BigBangAssessment.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AuthenticateDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStringForAuth")));
+
+builder.Services.AddDbContext<HotelRoomsContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStringForStudentTraining")));
+
+
+builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+builder.Services.AddScoped<HotelInterfaceRepo, HotelRepo>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+{
+    ValidateIssuerSigningKey = true,
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSetting:Token").Value)),
+    ValidateIssuer = false,
+    ValidateAudience = false,
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
